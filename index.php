33f162db5df1e3cf071ddcf5ff6417cd5fdd0a7d
adamol <adamol1992@gmail.com>
2018-07-24 19:11:19 +0200

    fix typo

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ $app = new Silex\Application;
 
 $app['debug'] = true;
 
-    $instance = parseRequest($request, Post::class);
 $app->post('posts', function(Request $request) {
+    $instance = parseRequest($request, Post::class);
 
     // do things with the parsed arguments
 
